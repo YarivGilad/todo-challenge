157638b5e08f41c7e7bf6b577c0da21d992681a7
SkinnyRibJohnson <roikohn@gmail.com>
2019-08-15 21:02:02 +0300

    initial

---
+++ src/components/todoitem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+function ToDoItem() {
+    
+    const [] = useState();
+  
+    return (
+      <div>
+      <button onClick={() => isCompleted()}>
+       completed
+      </button>
+        <p>todo list item recieved added by user</p>
+        <button>
+       remove
+      </button>
+      </div>
+    )
+}
+
+
+
