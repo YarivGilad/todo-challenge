327b11ed490f3521ac9b5758d45d09b2f6e27426
Yair Abranyi <yairabranyi@gmail.com>
2019-08-20 19:00:08 +0300

    final changing + style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import TodoItem from './TodoItem'
 
 function App() {
+  const add_todo = (item)=> {
+    console.log('itrm:',item);
+  }
   return (
     <div className="App">
-      <header className="App-header"><h1>Todo App</h1></header>
-      {/* <TodoItem/> */}
-      <h1>From App</h1>
+      {/* <header className="App-header"><h1>Todo App</h1></header> */}
+      <TodoItem add_todo={add_todo}/>
     </div>
   );
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,35 +1,71 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import PropTypes from 'prop-types';
 
 const Form = ({ add_todo }) => {
-  
-     const [todo_text,set_todo_text] = useState('');
 
-    const on_click = (event)=> {
-      event.preventDefault();
-      add_todo({
-          text: todo_text,
-          status : 'open'
-      })
-    }
+  const [todo_text, set_todo_text] = useState('');
 
-    // console.log('Text:',text)
-    return (
-      <div>
-         <h1>Hello from TodoItem component</h1> 
-         <form>
-           <input onChange={(event)=>set_todo_text(event.target.value)} type="text" placeholder="what needs to be done"></input>
-           <button onClick={on_click}>ADD</button>
-         </form>
+  const on_click = (event) => {
+    event.preventDefault();
+    add_todo({
+      text: todo_text,
+      status: 'open'
+    })
+  }
 
-      </div>
-    );
-  };
-  export default Form;
+  // console.log('Text:',text)
+  return (
+    <Warpper>
+      <h1>My todo Up</h1>
+      <TodoItem>
+        <input onChange={(event) => set_todo_text(event.target.value)} type="text" placeholder="what needs to be done"></input>
+        <button onClick={on_click}>ADD</button>
+      </TodoItem>
+    </Warpper>
+  );
+};
+export default Form;
 
 
-  Form.propTypes = {
-    add_todo: PropTypes.func.isRequired,
-  };
-  
+Form.propTypes = {
+  add_todo: PropTypes.func.isRequired,
+};
+
+const Warpper = styled.form`
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: auto;
+      /* width: 50%; */
+      margin: 10px 200px;
+      padding: 2px;
+      border: 2px gray solid;
+`;
+
+const TodoItem = styled.form`
+
+      display: flex;
+      justify-content: center;
+      margin: 10px;
+      color: papayawhip;
+      font-size: 10rem;
+      width: 50%;
+      background: slategray;
+
+`;
+
+
+
+
+
+// const Text = styled.h1`
+//   font-size: 3rem;
+//   font-weight: normal;
+//   font-family: "Griffy", cursive;
+//   color: papayawhip;
+//   cursor: pointer;
+//   user-select: none;
+//   position: relative;
+// `;
