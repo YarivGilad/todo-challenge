d9c9bb6316b47034dbcf329e555011368695405c
SkinnyRibJohnson <roikohn@gmail.com>
2019-08-20 14:41:38 +0300

    styled buttons

--- src/components/todoitem.js
+++ src/components/todoitem.js
@@ -5,19 +5,15 @@ import styled from 'styled-components';
 
 const ToDoItem = (props) => {
     
-  
-  
   const [itemState,setItemState] = useState(
     {item: props.item}
   )
-    
-  
 
     return (
       <div>
-      <button onClick={setItemState}>completed</button>
+      <CompleteButton onClick={setItemState}>completed</CompleteButton>
         <h1>{itemState.item}</h1>
-        <button>remove</button>
+        <RemoveButton>remove</RemoveButton>
       </div>
     )
 }
@@ -29,7 +25,7 @@ ToDoItem.propTypes = {
 
 export default ToDoItem;
 
-const button = styled.button`
+const CompleteButton = styled.button`
   background: #1DB954;
   color: white;
   display: inline-block;
@@ -38,4 +34,14 @@ const button = styled.button`
   margin: 0.5rem 1rem;
   width: 11rem;
   border: 2px solid #1DB954;
+`
+const RemoveButton = styled.button`
+  background: #ff0000;
+  color: white;
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  border: 2px solid #ff0000;
 `
